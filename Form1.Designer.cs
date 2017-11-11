31a0eb1206273312ea31361504ee1eb809c4db7d
lufy0538632202650 <lu.fy@biw.net.cn>
2017-11-11 13:26:38 +0800

    SADSD
    FFDFDDF

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace LFY_Bit8
+{
+	partial class Frm_BiwTestBit8
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.Tlp_MainTlp = new System.Windows.Forms.TableLayoutPanel();
+			this.Pnl_BytePnl = new System.Windows.Forms.Panel();
+			this.Pnl_OdPnl = new System.Windows.Forms.Panel();
+			this.Lbl_DLbl = new System.Windows.Forms.Label();
+			this.Lbl_OLbl = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.Btn_ElgBtn = new System.Windows.Forms.Button();
+			this.Btn_SevBtn = new System.Windows.Forms.Button();
+			this.Btn_SixBtn = new System.Windows.Forms.Button();
+			this.Btn_FivBtn = new System.Windows.Forms.Button();
+			this.Btn_FouBtn = new System.Windows.Forms.Button();
+			this.Btn_ThrBtn = new System.Windows.Forms.Button();
+			this.Btn_TwoBtn = new System.Windows.Forms.Button();
+			this.Btn_OneBtn = new System.Windows.Forms.Button();
+			this.Tlp_MainTlp.SuspendLayout();
+			this.Pnl_BytePnl.SuspendLayout();
+			this.Pnl_OdPnl.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// Tlp_MainTlp
+			// 
+			this.Tlp_MainTlp.ColumnCount = 2;
+			this.Tlp_MainTlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+			this.Tlp_MainTlp.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.Tlp_MainTlp.Controls.Add(this.Pnl_BytePnl, 0, 0);
+			this.Tlp_MainTlp.Controls.Add(this.Pnl_OdPnl, 1, 0);
+			this.Tlp_MainTlp.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Tlp_MainTlp.Location = new System.Drawing.Point(0, 0);
+			this.Tlp_MainTlp.Name = "Tlp_MainTlp";
+			this.Tlp_MainTlp.RowCount = 1;
+			this.Tlp_MainTlp.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.Tlp_MainTlp.Size = new System.Drawing.Size(598, 258);
+			this.Tlp_MainTlp.TabIndex = 0;
+			// 
+			// Pnl_BytePnl
+			// 
+			this.Pnl_BytePnl.Controls.Add(this.Btn_OneBtn);
+			this.Pnl_BytePnl.Controls.Add(this.Btn_TwoBtn);
+			this.Pnl_BytePnl.Controls.Add(this.Btn_ThrBtn);
+			this.Pnl_BytePnl.Controls.Add(this.Btn_FouBtn);
+			this.Pnl_BytePnl.Controls.Add(this.Btn_FivBtn);
+			this.Pnl_BytePnl.Controls.Add(this.Btn_SixBtn);
+			this.Pnl_BytePnl.Controls.Add(this.Btn_SevBtn);
+			this.Pnl_BytePnl.Controls.Add(this.Btn_ElgBtn);
+			this.Pnl_BytePnl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Pnl_BytePnl.Location = new System.Drawing.Point(3, 3);
+			this.Pnl_BytePnl.Name = "Pnl_BytePnl";
+			this.Pnl_BytePnl.Size = new System.Drawing.Size(472, 252);
+			this.Pnl_BytePnl.TabIndex = 0;
+			// 
+			// Pnl_OdPnl
+			// 
+			this.Pnl_OdPnl.Controls.Add(this.label2);
+			this.Pnl_OdPnl.Controls.Add(this.label1);
+			this.Pnl_OdPnl.Controls.Add(this.Lbl_DLbl);
+			this.Pnl_OdPnl.Controls.Add(this.Lbl_OLbl);
+			this.Pnl_OdPnl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.Pnl_OdPnl.Location = new System.Drawing.Point(481, 3);
+			this.Pnl_OdPnl.Name = "Pnl_OdPnl";
+			this.Pnl_OdPnl.Size = new System.Drawing.Size(114, 252);
+			this.Pnl_OdPnl.TabIndex = 1;
+			// 
+			// Lbl_DLbl
+			// 
+			this.Lbl_DLbl.AutoSize = true;
+			this.Lbl_DLbl.Location = new System.Drawing.Point(30, 195);
+			this.Lbl_DLbl.Name = "Lbl_DLbl";
+			this.Lbl_DLbl.Size = new System.Drawing.Size(11, 12);
+			this.Lbl_DLbl.TabIndex = 1;
+			this.Lbl_DLbl.Text = "0";
+			// 
+			// Lbl_OLbl
+			// 
+			this.Lbl_OLbl.AutoSize = true;
+			this.Lbl_OLbl.Location = new System.Drawing.Point(30, 92);
+			this.Lbl_OLbl.Name = "Lbl_OLbl";
+			this.Lbl_OLbl.Size = new System.Drawing.Size(11, 12);
+			this.Lbl_OLbl.TabIndex = 0;
+			this.Lbl_OLbl.Text = "0";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(16, 44);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 12);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "十进制";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(18, 152);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(53, 12);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "十六进制";
+			// 
+			// Btn_ElgBtn
+			// 
+			this.Btn_ElgBtn.Location = new System.Drawing.Point(9, 103);
+			this.Btn_ElgBtn.Name = "Btn_ElgBtn";
+			this.Btn_ElgBtn.Size = new System.Drawing.Size(50, 50);
+			this.Btn_ElgBtn.TabIndex = 0;
+			this.Btn_ElgBtn.Text = "0";
+			this.Btn_ElgBtn.UseVisualStyleBackColor = true;
+			this.Btn_ElgBtn.Click += new System.EventHandler(this.Btn_ElgBtn_Click);
+			// 
+			// Btn_SevBtn
+			// 
+			this.Btn_SevBtn.Location = new System.Drawing.Point(65, 103);
+			this.Btn_SevBtn.Name = "Btn_SevBtn";
+			this.Btn_SevBtn.Size = new System.Drawing.Size(50, 50);
+			this.Btn_SevBtn.TabIndex = 1;
+			this.Btn_SevBtn.Text = "0";
+			this.Btn_SevBtn.UseVisualStyleBackColor = true;
+			this.Btn_SevBtn.Click += new System.EventHandler(this.Btn_SevBtn_Click);
+			// 
+			// Btn_SixBtn
+			// 
+			this.Btn_SixBtn.Location = new System.Drawing.Point(121, 103);
+			this.Btn_SixBtn.Name = "Btn_SixBtn";
+			this.Btn_SixBtn.Size = new System.Drawing.Size(50, 50);
+			this.Btn_SixBtn.TabIndex = 2;
+			this.Btn_SixBtn.Text = "0";
+			this.Btn_SixBtn.UseVisualStyleBackColor = true;
+			this.Btn_SixBtn.Click += new System.EventHandler(this.Btn_SixBtn_Click);
+			// 
+			// Btn_FivBtn
+			// 
+			this.Btn_FivBtn.Location = new System.Drawing.Point(177, 103);
+			this.Btn_FivBtn.Name = "Btn_FivBtn";
+			this.Btn_FivBtn.Size = new System.Drawing.Size(50, 50);
+			this.Btn_FivBtn.TabIndex = 3;
+			this.Btn_FivBtn.Text = "0";
+			this.Btn_FivBtn.UseVisualStyleBackColor = true;
+			this.Btn_FivBtn.Click += new System.EventHandler(this.Btn_FivBtn_Click);
+			// 
+			// Btn_FouBtn
+			// 
+			this.Btn_FouBtn.Location = new System.Drawing.Point(233, 103);
+			this.Btn_FouBtn.Name = "Btn_FouBtn";
+			this.Btn_FouBtn.Size = new System.Drawing.Size(50, 50);
+			this.Btn_FouBtn.TabIndex = 4;
+			this.Btn_FouBtn.Text = "0";
+			this.Btn_FouBtn.UseVisualStyleBackColor = true;
+			this.Btn_FouBtn.Click += new System.EventHandler(this.Btn_FouBtn_Click);
+			// 
+			// Btn_ThrBtn
+			// 
+			this.Btn_ThrBtn.Location = new System.Drawing.Point(289, 103);
+			this.Btn_ThrBtn.Name = "Btn_ThrBtn";
+			this.Btn_ThrBtn.Size = new System.Drawing.Size(50, 50);
+			this.Btn_ThrBtn.TabIndex = 5;
+			this.Btn_ThrBtn.Text = "0";
+			this.Btn_ThrBtn.UseVisualStyleBackColor = true;
+			this.Btn_ThrBtn.Click += new System.EventHandler(this.Btn_ThrBtn_Click);
+			// 
+			// Btn_TwoBtn
+			// 
+			this.Btn_TwoBtn.Location = new System.Drawing.Point(345, 103);
+			this.Btn_TwoBtn.Name = "Btn_TwoBtn";
+			this.Btn_TwoBtn.Size = new System.Drawing.Size(50, 50);
+			this.Btn_TwoBtn.TabIndex = 6;
+			this.Btn_TwoBtn.Text = "0";
+			this.Btn_TwoBtn.UseVisualStyleBackColor = true;
+			this.Btn_TwoBtn.Click += new System.EventHandler(this.Btn_TwoBtn_Click);
+			// 
+			// Btn_OneBtn
+			// 
+			this.Btn_OneBtn.Location = new System.Drawing.Point(401, 103);
+			this.Btn_OneBtn.Name = "Btn_OneBtn";
+			this.Btn_OneBtn.Size = new System.Drawing.Size(50, 50);
+			this.Btn_OneBtn.TabIndex = 7;
+			this.Btn_OneBtn.Text = "0";
+			this.Btn_OneBtn.UseVisualStyleBackColor = true;
+			this.Btn_OneBtn.Click += new System.EventHandler(this.Btn_OneBtn_Click);
+			// 
+			// Frm_BiwTestBit8
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(598, 258);
+			this.Controls.Add(this.Tlp_MainTlp);
+			this.Name = "Frm_BiwTestBit8";
+			this.Tlp_MainTlp.ResumeLayout(false);
+			this.Pnl_BytePnl.ResumeLayout(false);
+			this.Pnl_OdPnl.ResumeLayout(false);
+			this.Pnl_OdPnl.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel Tlp_MainTlp;
+		private System.Windows.Forms.Panel Pnl_BytePnl;
+		private System.Windows.Forms.Panel Pnl_OdPnl;
+		private System.Windows.Forms.Label Lbl_DLbl;
+		private System.Windows.Forms.Label Lbl_OLbl;
+		private System.Windows.Forms.Button Btn_OneBtn;
+		private System.Windows.Forms.Button Btn_TwoBtn;
+		private System.Windows.Forms.Button Btn_ThrBtn;
+		private System.Windows.Forms.Button Btn_FouBtn;
+		private System.Windows.Forms.Button Btn_FivBtn;
+		private System.Windows.Forms.Button Btn_SixBtn;
+		private System.Windows.Forms.Button Btn_SevBtn;
+		private System.Windows.Forms.Button Btn_ElgBtn;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LFY_Bit8
+{
+	public partial class Frm_BiwTestBit8 : Form
+	{
+		byte var = 0;
+		public Frm_BiwTestBit8()
+		{
+			InitializeComponent();
+		}
+		private void NubBtnClick(Button btn)
+		{
+			if (btn.Text == "0")
+			{
+				btn.Text = "1";
+			}
+			else
+			{
+				btn.Text = "0";
+			}
+		}
+		private void ODLblChange(byte byt)
+		{
+			this.Lbl_OLbl.Text =byt.ToString();
+			this.Lbl_DLbl.Text = Convert.ToString(var,16);
+		}
+	     private void Btn_OneBtn_Click(object sender, EventArgs e)
+		{	
+			var = (Byte)(var^1);
+			NubBtnClick(Btn_OneBtn);
+			ODLblChange(var);
+		}
+
+		private void Btn_TwoBtn_Click(object sender, EventArgs e)
+		{
+			var = (Byte)(var^2);
+			NubBtnClick(Btn_TwoBtn);
+			ODLblChange(var);
+		}
+
+		private void Btn_ThrBtn_Click(object sender, EventArgs e)
+		{
+			var = (Byte)(var^4);
+			NubBtnClick(Btn_ThrBtn);
+			ODLblChange(var);
+		}
+
+		private void Btn_FouBtn_Click(object sender, EventArgs e)
+		{
+			var = (Byte)(var^8);
+			NubBtnClick(Btn_FouBtn);
+			ODLblChange(var);
+		}
+
+		private void Btn_FivBtn_Click(object sender, EventArgs e)
+		{
+			var = (Byte)(var^16);
+			NubBtnClick(Btn_FivBtn);
+			ODLblChange(var);
+		}
+
+		private void Btn_SixBtn_Click(object sender, EventArgs e)
+		{
+			var = (Byte)(var^32);
+			NubBtnClick(Btn_SixBtn);
+			ODLblChange(var);
+		}
+
+		private void Btn_SevBtn_Click(object sender, EventArgs e)
+		{
+			var = (Byte)(var^64);
+			NubBtnClick(Btn_SevBtn);
+			ODLblChange(var);
+		}
+
+		private void Btn_ElgBtn_Click(object sender, EventArgs e)
+		{
+			var = (Byte)(var^128);
+			NubBtnClick(Btn_ElgBtn);
+			ODLblChange(var);
+		}
+	}
+}
